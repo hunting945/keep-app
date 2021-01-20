c2920c4156b5fc19300120025a819595b69d5f5f
hunting945 <hunting945@livemail.tw>
2021-01-20 18:48:01 +0800

    add note component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Note from "./Note";
+
 
 function App() {
   return (
     <div>
       <Header />
       <h1>Change!</h1>
+      <Note title="testTitle" content="testContent"/>
       <Footer />
     </div>
   );
