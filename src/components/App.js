35a52ee0a49beca0b172932a16cf8943df8755e3
hunting945 <hunting945@livemail.tw>
2021-01-20 18:22:13 +0800

    add header, footer compoent and element

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
 function App() {
   return (
     <div>
+      <Header />
       <h1>Change!</h1>
+      <Footer />
     </div>
   );
 }
