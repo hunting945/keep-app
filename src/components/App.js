b63228e4f085b3ffb282c0c12f5dd3bef0440f45
hunting945 <hunting945@livemail.tw>
2021-01-21 17:17:50 +0800

    add delete button css

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import defaultNotes from "../notes"
+import defaultNotes from "../notes";
 
 
 function App() {
