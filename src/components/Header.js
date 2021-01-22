e629908fc50025683c85ed4fe10d2af4de18c283
hunterlai <hunter.lai.corp@outlook.com>
2021-01-23 00:53:27 +0800

    mod form css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 
 function Header() {
     return (
         <header>
-            <h1>Keep</h1>
+            <h1>
+                <EmojiObjectsIcon />
+                Keep
+            </h1>
         </header>
     );
 }
